0d0aabfe902450e7588f0f4de1215c70442ad7fb
Ananya <ananya2006garg@gmail.com>
2026-06-15 13:09:26 +0530

    add bubble

---
+++ bubble.py
@@ -0,0 +1,11 @@
+ar=[3,90,8,12,4]
+n=len(ar)
+for i in range(n-1):
+    for j in range(n-i-1):
+        if ar[j]>ar[j+1]:
+            ar[j],ar[j+1]=ar[j+1],ar[j]
+        else:
+            continue
+    print("after iteration: ", ar)
+
+print("sorted array: ", ar)
